f404b11ee7a14ba5f5ea3994cb4f14ed06bc0f9e
NicoPennec <nemohp@yahoo.fr>
2017-04-17 00:05:02 +0200

    Add raven-js client

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
-(function($) {
+(function($, Raven) {
+
+  Raven.config('https://aa63c1bdcb3d4f438df399d5c22c2f07@sentry.io/158785').install();
 
   $(function() {
     // init nav
     $(".button-collapse").sideNav();
   });
 
-})(jQuery);
+})(jQuery, Raven);
